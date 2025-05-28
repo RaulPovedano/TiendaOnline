0c2d43f27cb93a9747ef3d94483d11bc44eb3d00
__.raulpr__ <raulpovedano87@gmail.com>
2025-05-28 16:09:40 +0200

    correcion url backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://tiendaonline-qbuc.onrender.com/api',
   stripe: {
     publicKey: 'pk_test_51R8OWePoP3Gj5SX86xi0a9fU3Q3h6LoemxRBjRf4iSXEkDRQ08obgZAkI5YjZ1RX4gE6RT3Kh2PNpO9Bc3yxmoyz00UXnOMCHB'
   }
